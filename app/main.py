a9488c84084931e3c031e60cccee1ec2bcfd4ce1
Meir Assayag <assayag.meir@gmail.com>
2023-05-23 15:18:19 +0300

    [branch_library_system] Configure basic dockerfile for the rest api app and use docker compose to combine the api service with a postgres service. Use settings from some .env file to create the postgres url. Setup the sqlalchemy session. No models for now.

--- app/main.py
+++ app/main.py
@@ -1,6 +1,20 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, APIRouter, HTTPException, Depends, status, Request
+from fastapi.middleware.cors import CORSMiddleware
+from .database import engine
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/books")
